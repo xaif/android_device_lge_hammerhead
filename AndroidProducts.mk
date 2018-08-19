6c555644ad5a827340014936565c91a8ee6b8a9c
Xaif <huzaifaamirka46@gmail.com>
2018-08-19 21:40:57 +0530

    Hammerhead: Carbonify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,4 +17,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_hammerhead.mk \
     $(LOCAL_DIR)/full_hammerhead.mk \
-    $(LOCAL_DIR)/aosp.mk
+    $(LOCAL_DIR)/aosp.mk \
+    $(LOCAL_DIR)/carbon_hammerhead.mk
